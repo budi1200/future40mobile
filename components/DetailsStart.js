1df32d3685b9e2c77aee707c586e1942d5b1ba08
budi1200 <alenab22@gmail.com>
2018-10-13 23:07:52 +0200

    test fetching json

--- components/DetailsStart.js
+++ components/DetailsStart.js
@@ -9,11 +9,35 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
+import getXFromSheet from './future40_data';
+
 export default class DetailsStart extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    // Accespts sheet page name, returns state with data from google sheet
+    handleSheet = async(sheet) => {
+        getXFromSheet(sheet).then(result =>{
+            this.setState({
+                [sheet]: result,
+            });
+        })
+    }
+
+    // Calls needed sheets
+    componentWillMount() {
+        this.handleSheet("corporations");
+    }
+
     render() {
         return (
             <View>
-                <Text>World hello 321!</Text>
+                {!this.state.corporations ? <h3 style={animStyles.fadeIn}>Loading</h3> : 
+                                            this.state.corporations.data.corporations.map((corporation) => {
+                                                return(<Text>{corporation.name}</Text>)
+                                            })}
             </View>
         );
     }
