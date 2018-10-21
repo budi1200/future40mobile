53b3b0ce680d27d58de9f403579993729f3bfd5b
budi1200 <alenab22@gmail.com>
2018-10-21 17:49:53 +0200

    add momentjs, implement homepage

--- components/App.js
+++ components/App.js
@@ -7,13 +7,16 @@
  */
 
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, Image } from 'react-native';
 import { Navigation } from 'react-native-navigation';
+import axios from 'react-native-axios';
+import moment from 'moment';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 // Handle firebase connection
 import firebase from 'firebase';
 import { config } from '../fireConn';
+import { getSheetUrl } from './future40_data';
 import { addIconTopBar, handleButtonPress } from './customFunctions';
 firebase.initializeApp(config);
 
@@ -25,14 +28,22 @@ export default class App extends Component {
     Navigation.events().bindComponent(this);
 
     this.state = {
-      corporations: null
+      news: null
     }
+  }
 
+  handleSheet = (sheet) => {
+    getSheetUrl(sheet, (url) => {
+      axios.get(url).then(result => {
+        this.setState({
+            [sheet]: result.data[sheet],
+        })
+      })
+    })
   }
 
   // Handler for navigation button presses
   navigationButtonPressed({ buttonId }) {
-
     // Custom button handle function, accepts button id
     handleButtonPress(buttonId);
   }
@@ -56,14 +67,27 @@ export default class App extends Component {
   
   componentWillMount(){
     addIconTopBar("HomeScreen");
+    this.handleSheet("news");
   }
 
   render() {
     return (
-      <View>
-        <Text>Hello world 123!</Text>
-        <Text>v2.02122</Text>
-      </View>
+      <ScrollView>
+        <View><Text>Banner Placeholder</Text></View>
+
+  	  	{this.state.news == null ? <Text>Loading</Text> : this.state.news.map((news, index) => {
+          if(moment().isBetween(moment(news.show_from, "YYYY-MM-DD"), moment(news.show_to, "YYYY-MM-DD"), null, [])){
+            return(
+  	  	      <View key={index}>
+                <Text>{news.title}</Text>
+                <Image style={{ height: 200, width: 300, resizeMode: 'contain'}} source={{ uri: news.picture }}/>
+                <Text>{news.description}</Text>
+              </View>
+  	  	    )
+          }
+  	  	})}
+
+      </ScrollView>
     );
   }
 }
